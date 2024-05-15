e886bb84cda556bb6c7cfe06b2003dc2a238cdb2
Afaq Ahmad Rajpoot <ranag55db@gmail.com>
2024-05-16 00:28:37 +0500

    almost 70 percent is done. Dashboard UI. responsiveness is remains.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        whatsappGreen: '#075E54',
+      }
+    },
+  },
+  plugins: [],
+}
+
